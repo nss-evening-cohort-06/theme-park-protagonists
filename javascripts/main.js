1aaa14b42e8487f0ec4b404f71cb7cfba5a33fea
lewiskg <kenneth.g.lewis2@gmail.com>
2017-11-04 11:35:29 -0500

    cleaned up some stuff

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,12 @@
 'use strict';
 
 let events = require('./events');
+let apiKey = require('./apiKey');
 
 events.initialize();
 
 //grabs and formats current date and appends to footer
 const currentDate = moment().format("MMM DD, YYYY");
 $('#footer').append(currentDate);
-
-let apiKey = require('./apiKey');
 
 apiKey.retrieveKeys();
