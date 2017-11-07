17e2c6ae87667c0900b9ae3aea458bbcc14deeba
lewiskg <kenneth.g.lewis2@gmail.com>
2017-11-06 19:11:58 -0600

    minor deletions

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,8 +5,6 @@ const themePark = require('./themePark');
 const events = require('./events');
 const data = require('./data');
 
-events.initialize();
-
 //grabs and formats current date and appends to footer
 const currentDate = moment().format("MMM DD, YYYY");
 $('#footer').append(currentDate);
@@ -14,5 +12,3 @@ $('#footer').append(currentDate);
 apiKey.retrieveKeys();
 events.initialize();
 data.getAttractionsJSON();
-
-
